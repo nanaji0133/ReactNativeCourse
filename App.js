dcde2ff699decc280153e4a2fffced17eb602a56
nanaji0133 <nanajisanka5357@gmail.com>
2021-01-20 11:59:11 -0500

    react native dev setup

---
+++ App.js
@@ -0,0 +1,82 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+
+  ScrollView,
+
+
+  StatusBar, StyleSheet,
+
+
+  Text, View
+} from 'react-native';
+import {
+  Colors,
+  DebugInstructions, Header,
+  LearnMoreLinks,
+
+
+  ReloadInstructions
+} from 'react-native/Libraries/NewAppScreen';
+
+
+const App: () => React$Node = () => {
+  return (
+    <>
+     <View style={styles.sectionContainer}>
+       <Text>
+         Hello World
+       </Text>
+     </View>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  scrollView: {
+    backgroundColor: Colors.lighter,
+  },
+  engine: {
+    position: 'absolute',
+    right: 0,
+  },
+  body: {
+    backgroundColor: Colors.white,
+  },
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+    color: Colors.black,
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+    color: Colors.dark,
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+  footer: {
+    color: Colors.dark,
+    fontSize: 12,
+    fontWeight: '600',
+    padding: 4,
+    paddingRight: 12,
+    textAlign: 'right',
+  },
+});
+
+export default App;
