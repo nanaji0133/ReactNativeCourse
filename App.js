7f893a329d7f214a614b0df3ea93523b60f0abb0
nanaji0133 <nanajisanka5357@gmail.com>
2021-01-21 11:04:25 -0500

    stylesheet

--- App.js
+++ App.js
@@ -6,77 +6,46 @@
  * @flow strict-local
  */
 
-import React from 'react';
+import React, { useState } from 'react';
 import {
-  SafeAreaView,
+  StyleSheet,
 
-  ScrollView,
-
-
-  StatusBar, StyleSheet,
-
-
-  Text, View
+  Text,
+  View
 } from 'react-native';
-import {
-  Colors,
-  DebugInstructions, Header,
-  LearnMoreLinks,
 
 
-  ReloadInstructions
-} from 'react-native/Libraries/NewAppScreen';
 
+const App = () => {
 
-const App: () => React$Node = () => {
+  const [testColor, setTestColor] = useState("green");
+
   return (
     <>
-     <View style={styles.sectionContainer}>
-       <Text>
-         Hello World
-       </Text>
-     </View>
+      <View style={ styles.container }>
+        <View style={ styles.half1 }>
+        </View>
+        <View style={ styles.half2 }>
+
+        </View>
+      </View>
     </>
   );
 };
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
+  container: {
+    flex: 1,
+    flexDirection: "row"
   },
-  engine: {
-    position: 'absolute',
-    right: 0,
+  half1: {
+    flex: 1,
+    backgroundColor: "red"
   },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
+  half2:{
+    flex:1,
+    backgroundColor:"yellow"
+  }
 });
 
 export default App;
